e42e8e1b52f52adea12be1b59dc2cef9bc6a4809
Lucas Capoani <lucascapoani@hotmail.com>
2026-06-10 10:03:09 -0300

    feat(users): implementação do CRUD completo com sobrenome, status e lastAccess

--- backend-pool/src/modules/users/entities/user.entity.ts
+++ backend-pool/src/modules/users/entities/user.entity.ts
@@ -3,26 +3,40 @@ import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeor
 export enum UserRole {
   ADMIN = 'ADMIN',
   ESTOQUISTA = 'ESTOQUISTA',
-  PROFESSOR = 'PROFESSOR'
+  PROFESSOR = 'PROFESSOR',
+}
+
+export enum UserStatus {
+  ATIVO = 'ATIVO',
+  INATIVO = 'INATIVO',
 }
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column({ unique: true })
-  email: string;
+  email!: string;
 
   @Column()
-  password: string;
+  password!: string;
 
   @CreateDateColumn()
-  createdAt: Date;
+  createdAt!: Date;
 
   @Column()
-  name: string;
+  name!: string;
+
+  @Column({ nullable: true })
+  sobrenome?: string;
 
   @Column({ type: 'enum', enum: UserRole, default: UserRole.PROFESSOR })
-  role: UserRole;
+  role!: UserRole;
+
+  @Column({ type: 'enum', enum: UserStatus, default: UserStatus.ATIVO })
+  status!: UserStatus;
+
+  @Column({ nullable: true, type: 'timestamp' })
+  lastAccess?: Date;
 }
