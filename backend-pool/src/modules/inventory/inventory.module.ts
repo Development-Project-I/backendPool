4ac828a01d1d055d5bff55a54bf6b1ff62c46f2b
Roberto de Souza Camillo <153844542+robert0oi@users.noreply.github.com>
2026-06-11 18:55:11 +0000

    feat: impedir exclusão de itens do estoque vinculados a aulas cadastradas — PDD1-34

--- backend-pool/src/modules/inventory/inventory.module.ts
+++ backend-pool/src/modules/inventory/inventory.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 import { InventoryItem } from './entities/inventory.entity';
+import { AulaIngredient } from '../aulas/entities/aula-ingredient.entity'; // <-- Importe aqui
 
 @Module({
-  imports: [TypeOrmModule.forFeature([InventoryItem])],
+  imports: [TypeOrmModule.forFeature([InventoryItem, AulaIngredient])], 
   controllers: [InventoryController],
   providers: [InventoryService],
 })
-export class InventoryModule {};
+export class InventoryModule {}
